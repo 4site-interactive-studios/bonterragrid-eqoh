ef403e6b170e578a79c71affdc9650a8d704760e
bryancasler <bryan.casler@gmail.com>
2024-05-09 16:20:08 -0400

    Prettier

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import scss from "./sass/main.scss";
 window.nvtag_callbacks = window.nvtag_callbacks || {};
 var nvtag_callbacks = window.nvtag_callbacks;
 nvtag_callbacks.postRender = nvtag_callbacks.postRender || [];
-nvtag_callbacks.postRender.push(function(args) {
+nvtag_callbacks.postRender.push(function (args) {
   displayAccordion();
   mobileImage();
   bgImage();
@@ -23,72 +23,80 @@ nvtag_callbacks.postRender.push(function(args) {
 
   const target = document.querySelector(".form-item-selectamount");
   const config = { attributes: false, childList: true, subtree: true };
-  
-  const callback = function(mutationsList, observer) {
-    for(const mutation of mutationsList) {
-      if (mutation.type === 'childList') {
+
+  const callback = function (mutationsList, observer) {
+    for (const mutation of mutationsList) {
+      if (mutation.type === "childList") {
         selectAmount();
       }
     }
   };
   const observer = new MutationObserver(callback);
   observer.observe(target, config);
-
 });
 
 function getThemeOption(option) {
-  if(typeof fs_theme_options !== 'undefined' && fs_theme_options.hasOwnProperty(option)) {
+  if (
+    typeof fs_theme_options !== "undefined" &&
+    fs_theme_options.hasOwnProperty(option)
+  ) {
     return fs_theme_options[option];
   }
   return null;
 }
 
 function setEFTMessage() {
-  const message = getThemeOption('eft_message');
-  if(message) {    
-    const eftButtonWrapper = document.querySelector(".at-eft-button-wrapper label");
-    if(eftButtonWrapper) {
-      document.body.className += ' has-eft-message'; 
-      const messageMarkup = `<div class="eft-message">${message}</div>`; 
-      eftButtonWrapper.insertAdjacentHTML('afterend', messageMarkup);
+  const message = getThemeOption("eft_message");
+  if (message) {
+    const eftButtonWrapper = document.querySelector(
+      ".at-eft-button-wrapper label"
+    );
+    if (eftButtonWrapper) {
+      document.body.className += " has-eft-message";
+      const messageMarkup = `<div class="eft-message">${message}</div>`;
+      eftButtonWrapper.insertAdjacentHTML("afterend", messageMarkup);
     }
   }
 }
 
 function setFrequencyListeners() {
-  var frequencies = document.querySelectorAll('input[name="SelectedFrequency"]');
-  for(var i = 0; i < frequencies.length; i++) {
-    frequencies[i].addEventListener('change', function() {
+  var frequencies = document.querySelectorAll(
+    'input[name="SelectedFrequency"]'
+  );
+  for (var i = 0; i < frequencies.length; i++) {
+    frequencies[i].addEventListener("change", function () {
       updatePaymentMethods();
     });
   }
 }
 
 function updatePaymentMethods() {
-  var eft_field = document.querySelector('.at-eft-button-wrapper');
-  if(eft_field) {
-    var selected_frequency = document.querySelector('input[name="SelectedFrequency"]:checked');
-    if(selected_frequency.value === '0') { // give once
-      eft_field.style.display = 'none';
+  var eft_field = document.querySelector(".at-eft-button-wrapper");
+  if (eft_field) {
+    var selected_frequency = document.querySelector(
+      'input[name="SelectedFrequency"]:checked'
+    );
+    if (selected_frequency.value === "0") {
+      // give once
+      eft_field.style.display = "none";
     } else {
-      eft_field.style.display = 'block';
+      eft_field.style.display = "block";
     }
   }
 }
 
 function mobileImage() {
-  let img = document.querySelector('.responsive-hero img');
-  if (img && !img.getAttribute('src')) {
-    img.style.display = 'none';
+  let img = document.querySelector(".responsive-hero img");
+  if (img && !img.getAttribute("src")) {
+    img.style.display = "none";
   }
 }
 
 function showBody() {
-  setTimeout(function() {
+  setTimeout(function () {
     window.scrollTo(0, 0);
-    document.body.className += ' showBody'; 
+    document.body.className += " showBody";
   }, 750);
-  
 }
 
 /**
@@ -98,9 +106,9 @@ function showBody() {
 function displayAccordion() {
   let acc = document.getElementsByClassName("accordion");
   for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+    acc[i].addEventListener("click", function () {
       this.classList.toggle("active");
-  
+
       let panel = this.nextElementSibling;
       if (panel.style.display === "block") {
         panel.style.display = "none";
@@ -111,15 +119,15 @@ function displayAccordion() {
   }
 }
 
- /**
+/**
  * Background Image
  */
 function bgImage() {
-  const mainImage = getThemeOption('main_image_url');
-  if(mainImage) {
+  const mainImage = getThemeOption("main_image_url");
+  if (mainImage) {
     const bgImage = document.querySelector(".bg-image");
-    if(!bgImage) return;
-    bgImage.style.cssText = `background: url('${mainImage}'); background-repeat: no-repeat; background-size: cover; background-position: center center;`
+    if (!bgImage) return;
+    bgImage.style.cssText = `background: url('${mainImage}'); background-repeat: no-repeat; background-size: cover; background-position: center center;`;
   }
 }
 
@@ -127,28 +135,28 @@ function bgImage() {
  * Contribution Information - Select amount
  */
 function selectAmount() {
-  const labelAmount = document.querySelectorAll('.label-amount');
-  const labelOtheramount = document.querySelector('.edit-otheramount');
-  
-  labelOtheramount.addEventListener('focus', handleClick);
+  const labelAmount = document.querySelectorAll(".label-amount");
+  const labelOtheramount = document.querySelector(".edit-otheramount");
+
+  labelOtheramount.addEventListener("focus", handleClick);
 
   labelAmount.forEach((label, index) => {
     if (index === 0) {
       label.classList.add("active");
       label.querySelector('input[type="radio"]').checked = true;
-      labelOtheramount.removeAttribute('required');
-      labelOtheramount.value = '';
+      labelOtheramount.removeAttribute("required");
+      labelOtheramount.value = "";
     }
-    label.addEventListener('change', handleClick);
+    label.addEventListener("change", handleClick);
   });
 }
 
 function handleClick(e) {
-  const labelAmount = document.querySelectorAll('.label-amount');
+  const labelAmount = document.querySelectorAll(".label-amount");
   e.preventDefault();
-  labelAmount.forEach(label => {
+  labelAmount.forEach((label) => {
     const radio = label.querySelector('input[type="radio"]');
-    label.classList.remove('active');
+    label.classList.remove("active");
     if (e.currentTarget.parentNode.classList.contains("label-otheramount")) {
       if (radio.classList.contains("radio-other")) {
         radio.checked = true;
@@ -158,27 +166,30 @@ function handleClick(e) {
     }
   });
   if (e.currentTarget.parentNode.classList.contains("label-otheramount")) {
-    e.currentTarget.parentNode.classList.add('active');
+    e.currentTarget.parentNode.classList.add("active");
   } else {
-    e.currentTarget.classList.add('active');
+    e.currentTarget.classList.add("active");
   }
 }
-
 
 /**
  * Insert the premiums into the form
  */
 function insertPremiums() {
-  const premiums = getThemeOption('premiums');
-  if(Array.isArray(premiums)) {
-    const contributionInformation = document.querySelector(".ContributionInformation");
-    if(contributionInformation) {
-      premiums.forEach(function(premium, index) {
-        if(premium.color && premium.body && premium.title) {
-          const imageMarkup = (premium.image_url) ? `
+  const premiums = getThemeOption("premiums");
+  if (Array.isArray(premiums)) {
+    const contributionInformation = document.querySelector(
+      ".ContributionInformation"
+    );
+    if (contributionInformation) {
+      premiums.forEach(function (premium, index) {
+        if (premium.color && premium.body && premium.title) {
+          const imageMarkup = premium.image_url
+            ? `
               <div class="premium-image">
                 <img src="${premium.image_url}" border="0" alt="" title="" decoding="async" loading="lazy" />
-              </div>` : ``;
+              </div>`
+            : ``;
           const premiumMarkup = `
             <div class="premium color-${premium.color}">
               ${imageMarkup}
@@ -187,21 +198,25 @@ function insertPremiums() {
                 <p>${premium.body}</p>
               </div>
             </div>
-          `; 
-          contributionInformation.insertAdjacentHTML('beforeend', premiumMarkup);
+          `;
+          contributionInformation.insertAdjacentHTML(
+            "beforeend",
+            premiumMarkup
+          );
         }
       });
     }
   }
 }
 
-
 /**
  * Move elements from selected frequency radios
-*/
+ */
 
 function moveRadiosElements() {
-  const selectedFrequency = document.querySelector(".form-item-selectedfrequency");
+  const selectedFrequency = document.querySelector(
+    ".form-item-selectedfrequency"
+  );
   const radios = selectedFrequency.querySelector(".radios");
   const radioDescription = document.querySelector(".radio-description");
   const frequencyInput = selectedFrequency.querySelectorAll("label input");
@@ -209,98 +224,95 @@ function moveRadiosElements() {
 
   // Move Radio description outside radios div
   selectedFrequency.appendChild(radioDescription);
-  
+
   // Move inputs outside label and add id/for
   frequencyInput.forEach((input, index) => {
     const label = input.parentNode;
     const labelParent = label.parentNode;
-    input.setAttribute('id', 'id-' + index);
-    label.setAttribute('for', 'id-' + index);
+    input.setAttribute("id", "id-" + index);
+    label.setAttribute("for", "id-" + index);
     labelParent.insertBefore(input, label);
   });
 
   giveMonthly.textContent = "Give Monthly";
 
   // Add radios indicator div
-  radios.insertAdjacentHTML('beforeend', '<div class="radios__indicator"></div>');
+  radios.insertAdjacentHTML(
+    "beforeend",
+    '<div class="radios__indicator"></div>'
+  );
 }
 
 function checkboxRadiobutton() {
   /**
    * Add checkmark to checkboxes
    */
-  const checkboxes = document.querySelectorAll('label.at-check');
+  const checkboxes = document.querySelectorAll("label.at-check");
 
-  checkboxes.forEach(checkbox => {
-    checkbox.insertAdjacentHTML('beforeend', '<div class="checkmark"></div>');
+  checkboxes.forEach((checkbox) => {
+    checkbox.insertAdjacentHTML("beforeend", '<div class="checkmark"></div>');
   });
 
   /**
-  * Add checkmark to radio buttons
-  */
+   * Add checkmark to radio buttons
+   */
 
   const radioButtons = document.querySelectorAll("[class^='at-radio']");
 
-  radioButtons.forEach(radio => {
-    radio.insertAdjacentHTML('beforeend', '<div class="checkmark"></div>');
+  radioButtons.forEach((radio) => {
+    radio.insertAdjacentHTML("beforeend", '<div class="checkmark"></div>');
   });
 }
- 
 
 /**
-* Intersection Observer viewport & scroll into view
-*/
-
+ * Intersection Observer viewport & scroll into view
+ */
 
 function isViewport() {
-
-  const takeAction = document.querySelector('.take-action');
+  const takeAction = document.querySelector(".take-action");
   if (!takeAction) return;
 
-  if (takeAction.innerHTML.trim() !== '') {
-    const target = document.querySelector('.ContributionInformation .radios');
-    const config = { rootMargin: '0px', threshold: 1.0 }
+  if (takeAction.innerHTML.trim() !== "") {
+    const target = document.querySelector(".ContributionInformation .radios");
+    const config = { rootMargin: "0px", threshold: 1.0 };
 
     let callback = (entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting || (entry.boundingClientRect.top <= 0)) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting || entry.boundingClientRect.top <= 0) {
           takeAction.style.display = "none";
         } else {
           takeAction.style.display = "";
         }
       });
     };
-    
+
     const observer = new IntersectionObserver(callback, config);
     observer.observe(target);
   } else {
     takeAction.style.display = "none";
   }
-
 }
 
 function takeActionScroll() {
-  const takeAction = document.querySelector('.take-action');
+  const takeAction = document.querySelector(".take-action");
   if (!takeAction) return;
 
-  if (takeAction.innerHTML.trim() !== '') {
-    takeAction.addEventListener('click', function (e) {
+  if (takeAction.innerHTML.trim() !== "") {
+    takeAction.addEventListener("click", function (e) {
       e.preventDefault();
-      document.querySelector('.ContributionInformation').scrollIntoView({
-          behavior: 'smooth'
+      document.querySelector(".ContributionInformation").scrollIntoView({
+        behavior: "smooth",
       });
     });
   }
-
 }
 
 function nextButton() {
   const nextButton = document.querySelector(".nextStep");
   if (!nextButton) return;
 
-  nextButton.innerHTML = 'Continue';    
+  nextButton.innerHTML = "Continue";
 }
-
 
 function photoCredit() {
   const bgImage = document.querySelector(".bg-image");
@@ -311,7 +323,13 @@ function photoCredit() {
   const responsiveHeroImage = responsiveHero.querySelector("img");
   if (responsiveHeroImage && responsiveHeroImage.hasAttribute("title")) {
     const photoAttribute = responsiveHeroImage.getAttribute("title");
-    bgImage.insertAdjacentHTML('beforeend', `<div class="photoCredit">${photoAttribute}</div>`);
-    responsiveHero.insertAdjacentHTML('beforeend', `<div class="photoCredit">${photoAttribute}</div>`);
+    bgImage.insertAdjacentHTML(
+      "beforeend",
+      `<div class="photoCredit">${photoAttribute}</div>`
+    );
+    responsiveHero.insertAdjacentHTML(
+      "beforeend",
+      `<div class="photoCredit">${photoAttribute}</div>`
+    );
   }
 }
